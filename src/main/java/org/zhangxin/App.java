34fed232bc23223ec996c7282c681662de4715b2
张新 <1367800014@qq.com>
2020-06-21 13:19:10 +0800

    123

---
+++ src/main/java/org/zhangxin/App.java
@@ -0,0 +1,13 @@
+package org.zhangxin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
