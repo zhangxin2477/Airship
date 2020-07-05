ae0be59da4195223876173b943928d63cf7fee3c
张新 <1367800014@qq.com>
2020-07-05 20:39:57 +0800

    2020年07月05日20:39:54
    123

---
+++ api/src/test/java/com/zhangxin/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangxin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
