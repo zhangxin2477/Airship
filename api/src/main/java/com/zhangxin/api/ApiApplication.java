f62a6b23b443f10ecf0d6019f209ebba31fcd016
张新 <1367800014@qq.com>
2020-07-19 23:11:31 +0800

    2020年07月19日23:11:28

--- api/src/main/java/com/zhangxin/api/ApiApplication.java
+++ api/src/main/java/com/zhangxin/api/ApiApplication.java
@@ -1,7 +1,9 @@
 package com.zhangxin.api;
 
 import com.zhangxin.api.hadoop.HdfsService;
+import com.zhangxin.api.kafka.KafkaService;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.Properties;
 
 @SpringBootApplication
 public class ApiApplication {
-    private final String hdfsUri = "hdfs://172.16.51.128:9000";
 
     @Resource
     private RestTemplateBuilder restTemplateBuilder;
@@ -31,8 +33,15 @@ public class ApiApplication {
     public HdfsService getHdfsService() {
         BasicConfigurator.configure();
         Configuration configuration = new Configuration();
+        String hdfsUri = "hdfs://172.16.51.128:9000";
         configuration.set("fs.defaultFS", hdfsUri);
         return new HdfsService(configuration, hdfsUri);
     }
 
+    @Bean
+    public KafkaService getKafkaService() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.51.128:9092,172.16.51.129:9092,172.16.51.131:9092,172.16.51.132:9092");
+        return new KafkaService(properties);
+    }
 }
--- api/src/test/java/com/zhangxin/api/ApiApplicationTests.java
+++ api/src/test/java/com/zhangxin/api/ApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.zhangxin.api;
 
 import com.zhangxin.api.hadoop.HdfsService;
+import com.zhangxin.api.kafka.KafkaService;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,18 @@ class ApiApplicationTests {
         log.info("{}", hdfsService.getFiles("/zxnode_test", null));
         log.info("{}", hdfsService.openWithString("/zxnode_test/nginx.conf"));
         hdfsService.download("/zxnode_test/nginx.conf", "/Users/zhangxin/test.txt");
-    }
+        log.info("{}", (Object[]) hdfsService.getFileBlockLocations("/zxnode_test/nginx.conf"));
 
+        kafkaService.createTopic("zxnode_test");
+        log.info("{}", kafkaService.getTopics().names());
+        kafkaService.deal("zxnode_test", "test");
+        for (int i = 0; i < 4; i++) {
+            kafkaService.send("zxnode_test", "test" + i);
+            try {
+                Thread.sleep(5 * 1000);
+            } catch (InterruptedException e) {
+                log.error("", e);
+            }
+        }
+    }
 }
