f1a7b89f1c001149da0abf2eaff5d346d6e511ef
张新 <1367800014@qq.com>
2020-07-19 18:26:16 +0800

    2020年07月19日18:26:12
    2020年07月19日18:26:05

--- api/src/main/java/com/zhangxin/api/ApiApplication.java
+++ api/src/main/java/com/zhangxin/api/ApiApplication.java
@@ -1,13 +1,38 @@
 package com.zhangxin.api;
 
+import com.zhangxin.api.hadoop.HdfsService;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class ApiApplication {
+    private final String hdfsUri = "hdfs://172.16.51.128:9000";
+
+    @Resource
+    private RestTemplateBuilder restTemplateBuilder;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public HdfsService getHdfsService() {
+        BasicConfigurator.configure();
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", hdfsUri);
+        return new HdfsService(configuration, hdfsUri);
     }
 
 }
--- api/src/test/java/com/zhangxin/api/ApiApplicationTests.java
+++ api/src/test/java/com/zhangxin/api/ApiApplicationTests.java
@@ -1,13 +1,26 @@
 package com.zhangxin.api;
 
+import com.zhangxin.api.hadoop.HdfsService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class ApiApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(ApiApplicationTests.class);
+    @Resource
+    private HdfsService hdfsService;
 
     @Test
     void contextLoads() {
+        log.info("{}", hdfsService.mkdir("/zxnode_test"));
+        hdfsService.upload(false, true, "/Users/zhangxin/nginx.conf", "/zxnode_test");
+        log.info("{}", hdfsService.getFiles("/zxnode_test", null));
+        log.info("{}", hdfsService.openWithString("/zxnode_test/nginx.conf"));
+        hdfsService.download("/zxnode_test/nginx.conf", "/Users/zhangxin/test.txt");
     }
 
 }
